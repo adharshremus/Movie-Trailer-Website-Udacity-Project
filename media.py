da5f1bdc3102c74b3cfa02924e2d80e85715a63a
Adharsh A <42710464+adharshremus@users.noreply.github.com>
2018-08-27 09:39:47 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,11 @@
 import webbrowser
 class Movie():
+    """ 'self' keyword we can access the attributes and methods of the class in python for __init__
+    Arguments:
+    parameter 1: address the movie_title 
+    parameter 2: pops up the storyline with respective movie
+    parameter 3: shows poster of the movie
+    parameter 4: URL for trailer of the movie """
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
