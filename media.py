9ed7c20b76d8e6c99f16a02fdedaa140327e3290
Adharsh A <42710464+adharshremus@users.noreply.github.com>
2018-08-27 18:53:11 +0530

    Update media.py

--- media.py
+++ media.py
@@ -5,13 +5,18 @@ class Movie():
     parameter 1: address the movie_title 
     parameter 2: pops up the storyline with respective movie
     parameter 3: shows poster of the movie
-    parameter 4: URL for trailer of the movie """
+    parameter 4: URL for trailer of the movie
+    """
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        """ movie name """
         self.title = movie_title
+        """ movie storyline """
         self.storyline = movie_storyline
+        """ movie poster image url to access """
         self.poster_image_url = poster_image
+        """ movie trailer url to access """
         self.trailer_youtube_url = trailer_youtube
-      
+     """self - access the python classes"""
     def show_trailer(self):
         
         webbrowser.open(self.trailer_youtube_url)
